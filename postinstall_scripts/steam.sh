3fe0f0e1aa52541971c54fec051456b8a8ffff64
arkadysmith <nw2025@protonmail.com>
2025-08-14 12:50:49 +0000

    Update steam.sh

--- postinstall_scripts/steam.sh
+++ postinstall_scripts/steam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 &&
+sudo apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 sudo apt install steam-installer
