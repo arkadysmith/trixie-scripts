fd57ba75bc7bad6bb0699d91b39b0e1be6a818e6
arkadysmith <nw2025@protonmail.com>
2025-08-14 14:30:52 +0000

    Update java8.sh

--- postinstall_scripts/java8.sh
+++ postinstall_scripts/java8.sh
@@ -3,6 +3,6 @@ sudo mkdir -p /etc/apt/keyrings
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update
-sudo apt install temurin-8-jdk
+sudo apt install -y temurin-8-jdk
 
 
