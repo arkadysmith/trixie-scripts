2621e7484820aae67ac7295b7b8e3fff0eaef614
arkadysmith <nw2025@protonmail.com>
2025-08-14 12:51:41 +0000

    Update musthave.sh

--- postinstall_scripts/musthave.sh
+++ postinstall_scripts/musthave.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-sudo apt install \
-	curl	
-	fastfetch
-	timeshift
-	btop	
-	gimp
-	scrot
-	gparted
-	gnome-disk-utility
-	galculator
-	vlc
-	qbittorrent
-	obs-studio
-	libreoffice
-	audacity
+sudo apt install curl fastfetch timeshift btop gimp scrot gparted gnome-disk-utility galculator vlc qbittorrent obs-studio libreoffice audacity
 	
