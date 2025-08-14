54a339220f8719103cb11bdb0c321c66964ff0a7
arkadysmith <nw2025@protonmail.com>
2025-08-14 11:57:04 +0000

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash ~/trixie-scripts/postinstall_scripts/musthave.sh
-bash ~/trixie-scripts/postinstall_scripts/winehq_trixie
+bash ~/trixie-scripts/postinstall_scripts/winehq_trixie.sh
 bash ~/trixie-scripts/postinstall_scripts/steam.sh
 bash ~/trixie-scripts/postinstall_scripts/java8.sh
