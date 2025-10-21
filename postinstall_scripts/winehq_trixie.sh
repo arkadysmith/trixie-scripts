9df3a4c764c8e737336bac8f1fe3db120e84e31c
arkadysmith <nw2025@protonmail.com>
2025-10-21 20:20:41 +0000

    Update winehq_trixie.sh

--- postinstall_scripts/winehq_trixie.sh
+++ postinstall_scripts/winehq_trixie.sh
@@ -4,4 +4,5 @@ sudo mkdir -pm755 /etc/apt/keyrings
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/trixie/winehq-trixie.sources
 sudo apt update
-sudo apt install winehq-staging=9.21~trixie-1 wine-staging=9.21~trixie-1 wine-staging-amd64=9.21~trixie-1 wine-staging-i386=9.21~trixie-1
+sudo apt install -y winehq-staging=9.21~trixie-1 wine-staging=9.21~trixie-1 wine-staging-amd64=9.21~trixie-1 wine-staging-i386=9.21~trixie-1
+sudo apt-mark hold wine-staging-amd64 wine-staging-i386 wine-staging winehq-staging
